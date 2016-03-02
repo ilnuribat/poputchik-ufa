3da5428285033e08b3228643e9877f40acdc358d
Ильнур Ибатуллин < >
2016-03-03 00:48:57 +0500

    create two tables

---
+++ createDB.sql
@@ -0,0 +1,22 @@
+DROP DATABASE IF EXISTS poputchik_ufa;
+CREATE DATABASE poputchik_ufa CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE poputchik_ufa;
+
+CREATE TABLE `users` (
+	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`name` TEXT,
+	`phone` VARCHAR(10),
+	`pin` VARCHAR(4)
+);
+
+CREATE TABLE `auth_users` (
+	`id_user` INT NOT NULL PRIMARY KEY,
+	`generated_id` VARCHAR(10) UNIQUE,
+	`hash` TEXT,
+	FOREIGN KEY (`id_user`) REFERENCES `users`(`id`)
+);
+
+INSERT INTO users (id, name, phone, pin) VALUES(1, "Ilnur", "9874774911", "1234");
+INSERT INTO auth_users(id_user, generated_id, hash) VALUES(1, "1234567890", MD5("1234567890" + "1234"));
+
+SELECT hash FROM auth_users;
