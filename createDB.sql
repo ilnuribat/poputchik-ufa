0d1f6f79a979f09d5fde1586bdcc03cd931ddff9
ilnuribat <ilnuribat@gmail.com>
2016-03-28 19:32:46 +0500

    create new row - id, id_user now is not primary key

--- createDB.sql
+++ createDB.sql
@@ -4,19 +4,21 @@ USE poputchik_ufa;
 
 CREATE TABLE `users` (
 	`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	`login` TEXT,
 	`name` TEXT,
-	`phone` VARCHAR(10),
-	`pin` VARCHAR(4)
+	`pin` INT,
+	`phone` VARCHAR(10)
 );
 
 CREATE TABLE `auth_users` (
-	`id_user` INT PRIMARY KEY,
+	`id` INT PRIMARY KEY AUTO_INCREMENT,
 	`generated_id` VARCHAR(10) UNIQUE,
+	`id_user` INT,
 	`hash` TEXT,
 	FOREIGN KEY (`id_user`) REFERENCES `users`(`id`)
 );
 
-INSERT INTO users (id, name, phone, pin) VALUES(1, "Ilnur", "9874774911", "1234");
+INSERT INTO users (id, name, phone, pin) VALUES(1, "Ilnur", "9874774911", 1234);
 INSERT INTO auth_users(id_user, generated_id, hash) VALUES(1, "1234567890", MD5("1234567890" + "1234"));
 
 SELECT hash FROM auth_users;
